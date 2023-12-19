04ad435aa8535c890b507c9396db37bcd2cba661
CJ Holmes <cholmes@lear.com>
2023-12-19 08:36:08 -0800

    Trivial container, layout, and nav things

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react"
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Us">
+      <p>Mister Bongosian is the best.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <title>About Us</title>
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import * as React from "react"
+import Layout from '../components/layout'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby tutorial</p>
+    </Layout>
+  )
 }
+
+export const Head = () => <title>Home Page</title>
+
+export default IndexPage
