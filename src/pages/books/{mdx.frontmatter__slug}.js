fae56d4e3c1df820f606aa60c425a0b28560bdf1
CJ Holmes <cholmes@lear.com>
2023-12-26 11:55:54 -0800

    PR16 Connect book detail page to book data

--- src/pages/books/{mdx.frontmatter__slug}.js
+++ src/pages/books/{mdx.frontmatter__slug}.js
@@ -1,15 +1,41 @@
 import * as React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
-const BookPage = () => {
+const BookPage = ({data, children}) => {
   return (
-    <Layout pageTitle="Stuff about a book">
-      <p>So much book.</p>
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <div class="row">
+        <div class="col-2">
+
+        </div>
+        <div class="col-2">
+          {children}
+        </div>
+      </div>
     </Layout>
   )
 }
 
-export const Head = () => <Seo title="Super Cool Blog Posts" />
+export const query = graphql`
+query ($id: String) {
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      author
+      releaseDate(formatString: "MMM DD, YYYY")
+      slug
+      square
+      subtitle
+      title
+      isbns { ebook paperback }
+    }
+    id
+    body
+  }
+}
+`
+
+export const Head = ({data}) => <Seo title={data.mdx.frontmatter.title} />
 
 export default BookPage
